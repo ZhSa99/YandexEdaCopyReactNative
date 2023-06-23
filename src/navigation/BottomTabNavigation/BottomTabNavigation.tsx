7cc2400f37237af007b897e74901fb65686dfc8e
ZhSa99 <zhsa.09.99@gmail.com>
2023-06-24 04:29:12 +0600

    added screens to the bottom tab navigator

--- src/navigation/BottomTabNavigation/BottomTabNavigation.tsx
+++ src/navigation/BottomTabNavigation/BottomTabNavigation.tsx
@@ -1,15 +1,26 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { BottomTabRootParamList } from '../types'
+import MainScreen from '../../screens/MainScreen/MainScreen'
+import RestaurantsScreen from '../../screens/RestaurantsScreen/RestaurantsScreen'
+import ShopsScreen from '../../screens/ShopsScreen/ShopsScreen'
+import BasketScreen from '../../screens/BasketScreen/BasketScreen'
 
-const BottomTab = createBottomTabNavigator()
+const BottomTab = createBottomTabNavigator<BottomTabRootParamList>()
 
 const BottomTabNavigation = () => {
-  return (
-    <BottomTab.Navigator>
-      
-    </BottomTab.Navigator>
-  )
+	return (
+		<BottomTab.Navigator>
+			<BottomTab.Screen name={'MainScreen'} component={MainScreen} />
+			<BottomTab.Screen
+				name={'RestaurantsScreen'}
+				component={RestaurantsScreen}
+			/>
+			<BottomTab.Screen name={'ShopsScreen'} component={ShopsScreen} />
+			<BottomTab.Screen name={'BasketScreen'} component={BasketScreen} />
+		</BottomTab.Navigator>
+	)
 }
 
 export default BottomTabNavigation
