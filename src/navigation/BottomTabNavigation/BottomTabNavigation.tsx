4f28e2d90d3423505d55b9728909f245cac628f5
ZhSa99 <zhsa.09.99@gmail.com>
2023-06-24 04:19:14 +0600

    added bottomTab navigator

---
+++ src/navigation/BottomTabNavigation/BottomTabNavigation.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+
+const BottomTab = createBottomTabNavigator()
+
+const BottomTabNavigation = () => {
+  return (
+    <BottomTab.Navigator>
+      
+    </BottomTab.Navigator>
+  )
+}
+
+export default BottomTabNavigation
+
+const styles = StyleSheet.create({})
