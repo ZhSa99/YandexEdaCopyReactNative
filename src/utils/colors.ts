d5fa7706b01a2be304ceabcaefc637763c66e8e0
ZhSa99 <zhsa.09.99@gmail.com>
2023-06-26 04:15:38 +0600

    create colors file

---
+++ src/utils/colors.ts
@@ -0,0 +1,11 @@
+export const buttonColors = {
+	activeColor: '#d7d6d2',
+	inactiveColor: '#7e7c79',
+}
+
+export const bottomTabColors = {
+	bottomTabColor: '#41413e',
+	sceneBackgroundColor: '#191918',
+}
+
+export const tabBarBackgroundColor = '#41413e'
