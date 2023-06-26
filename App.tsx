948bfc7b48810e257beabc242f0aacd9aa3313df
ZhSa99 <zhsa.09.99@gmail.com>
2023-06-26 16:32:23 +0600

    add status bar

--- App.tsx
+++ App.tsx
@@ -1,14 +1,20 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { Dimensions, StyleSheet, Text, View } from 'react-native'
 import BottomTabNavigation from './src/navigation/BottomTabNavigation/BottomTabNavigation'
 import 'react-native-gesture-handler'
 import DrawerNavigation from './src/navigation/DrawerNavigation/DrawerNavigation'
 
+const {width, height} = Dimensions.get('screen')
+
 export default function App() {
+
 	return (
-		<NavigationContainer>
-			<DrawerNavigation />
-		</NavigationContainer>
+		<View style={{width, height}}>
+			<StatusBar style='light'/>
+			<NavigationContainer>
+				<DrawerNavigation />
+			</NavigationContainer>
+		</View>
 	)
 }
