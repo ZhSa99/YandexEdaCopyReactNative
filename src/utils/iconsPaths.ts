b53a50722c4b197ab4d61c0652eb0a912ff5d456
ZhSa99 <zhsa.09.99@gmail.com>
2023-06-27 20:51:22 +0600

    add paths file

---
+++ src/utils/iconsPaths.ts
@@ -0,0 +1,12 @@
+// BOTTOM TAB ICONS
+//
+export const MainScreen_ICON = require('../../assets/icons/mainScreen_icon.png')
+export const RestaurantsScreen_ICON = require('../../assets/icons/restaurantsScreen_icon.png')
+export const ShopsScreen_ICON = require('../../assets/icons/shopsScreen_icon.png')
+export const BasketScreen_ICON = require('../../assets/icons/basketScreen_icon.png')
+
+// DRAWER NAVIGATION ICONS
+//
+export const user_ICON = require('../../assets/icons/user_icon.png')
+export const yandexPlus_ICON = require('../../assets/icons/yandexPlus_icon.png')
+
