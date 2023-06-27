c18419f009f6a2bcfb14a92983b33d0d615ab88e
ZhSa99 <zhsa.09.99@gmail.com>
2023-06-27 20:51:03 +0600

    add new constants

--- src/utils/globalConstants.ts
+++ src/utils/globalConstants.ts
@@ -1,4 +1,14 @@
+import { scale } from "react-native-size-matters"
+
 export const bottomTabElemSizes = {
   tabBarLabelFontSize: 10.5,
   bottomNavigatorHeight: 70
+}
+
+export const drawerElemSizes = {
+	headerLabelFontSize: {
+		first: scale(13),
+		second: scale(15.5),
+	},
+	yandexBonusFontSize: scale(18),
 }
