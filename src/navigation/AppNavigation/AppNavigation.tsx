6d50086d4617f5544dd0be88cea64b10886adb81
ZhSa99 <zhsa.09.99@gmail.com>
2023-06-30 03:20:59 +0600

    some changes

--- src/navigation/AppNavigation/AppNavigation.tsx
+++ src/navigation/AppNavigation/AppNavigation.tsx
@@ -5,41 +5,41 @@ import StackNavigation from '../StackNavigation/StackNavigation'
 import { scale } from 'react-native-size-matters'
 import { DrawerContext } from '../../context/DrawerContext/DrawerContext'
 import { StackRootParamList } from '../types'
+import { NavigationContext } from '../../context/NavigationContext/NavigationContext'
 
-const AppNavigation = React.forwardRef(
-	(props, ref: ForwardedRef<NavigationContainerRef<StackRootParamList>>) => {
-		const { drawerIsOpen } = useContext(DrawerContext)
+const AppNavigation = () => {
+	const { drawerIsOpen } = useContext(DrawerContext)
+	const {navigationRef} = useContext(NavigationContext)
 
-		const scrollX = useRef(new Animated.Value(0)).current
+	const scrollX = useRef(new Animated.Value(0)).current
 
-		const slideAnimation = () => {
-			Animated.timing(scrollX, {
-				toValue: drawerIsOpen ? scale(270) : 0,
-				duration: 300,
-				useNativeDriver: true,
-			}).start()
-		}
-
-		useEffect(() => {
-			slideAnimation()
-		}, [drawerIsOpen])
-
-		return (
-			<Animated.View
-				style={[
-					{ width: '100%', height: '100%', zIndex: 1 },
-					{
-						transform: [{ translateX: scrollX }],
-					},
-				]}
-			>
-				<NavigationContainer ref={ref}>
-					<StackNavigation />
-				</NavigationContainer>
-			</Animated.View>
-		)
+	const slideAnimation = () => {
+		Animated.timing(scrollX, {
+			toValue: drawerIsOpen ? scale(270) : 0,
+			duration: 300,
+			useNativeDriver: true,
+		}).start()
 	}
-)
+
+	useEffect(() => {
+		slideAnimation()
+	}, [drawerIsOpen])
+
+	return (
+		<Animated.View
+			style={[
+				{ width: '100%', height: '100%', zIndex: 1 },
+				{
+					transform: [{ translateX: scrollX }],
+				},
+			]}
+		>
+			<NavigationContainer ref={navigationRef}>
+				<StackNavigation />
+			</NavigationContainer>
+		</Animated.View>
+	)
+}
 
 export default AppNavigation
 
