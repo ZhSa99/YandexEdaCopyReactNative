f09701e31177cb76109d4a68794acde33058060e
ZhSa99 <zhsa.09.99@gmail.com>
2023-06-30 03:21:54 +0600

    add screen names

---
+++ src/utils/screenLabels.ts
@@ -0,0 +1,9 @@
+export const screenNames_RU = {
+	ordersList: 'Заказы',
+	profile: 'Профиль',
+	promocodes: 'Промокоды',
+	addresses: 'Адреса',
+	aboutService: 'О сервисе',
+	becomeCourier: 'Стать курьером',
+	foodForBusiness: 'Еда для бизнеса'
+}
