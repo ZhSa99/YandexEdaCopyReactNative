8c687846496c788a303e355c3eba77baca81f80a
ZhSa99 <zhsa.09.99@gmail.com>
2023-06-30 03:19:20 +0600

    add new function

--- src/context/DrawerContext/DrawerContext.tsx
+++ src/context/DrawerContext/DrawerContext.tsx
@@ -6,18 +6,15 @@ interface IDrawerProvider {
 }
 interface IDrawerContext {
 	drawerIsOpen: boolean | undefined
-	setDrawerIsOpen: (val: boolean | undefined | GestureResponderEvent) => void
+	setDrawerIsOpen: () => void
 }
 
 export const DrawerContext = createContext({} as IDrawerContext)
 
 export const DrawerProvider = ({ children }: IDrawerProvider) => {
 	const [drawerIsOpen, setDrawerIsOpen] = useState<boolean | undefined>(false)
-
-	const drawerHandler = (
-		value: boolean | undefined | GestureResponderEvent
-	) => {
-		setDrawerIsOpen(value && !drawerIsOpen)
+	const drawerHandler = () => {
+		setDrawerIsOpen(!drawerIsOpen)
 	}
 
 	const value = {
