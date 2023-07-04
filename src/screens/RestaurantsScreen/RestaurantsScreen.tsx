4b47ffdee5c4c37024a6f66d78f7dd25f0452b9b
ZhSa99 <zhsa.09.99@gmail.com>
2023-07-04 07:48:18 +0600

    add test elements

--- src/screens/RestaurantsScreen/RestaurantsScreen.tsx
+++ src/screens/RestaurantsScreen/RestaurantsScreen.tsx
@@ -1,12 +1,40 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import HeaderAndSearchWIthChildren from '../../customElements/HeaderAndSearchWithChildren/HeaderAndSearchWithChildren'
+import { bottomTabColors } from '../../utils/colors'
+import { scale, verticalScale } from 'react-native-size-matters'
 
 const RestaurantsScreen = () => {
   return (
-    <View>
-      <Text>RestaurantsScreen</Text>
-    </View>
-  )
+		<SafeAreaView>
+			<HeaderAndSearchWIthChildren
+				children={new Array(20)
+					.fill({
+						test: 'test',
+						key: Math.random(),
+					})
+					.map((elem, index) => {
+						return (
+							<View
+								key={index}
+								style={{
+									width: '100%',
+									height: verticalScale(100),
+									backgroundColor: bottomTabColors.bottomTabColor,
+									marginTop: index !== 0 ? 10 : 0,
+									alignItems: 'center',
+									justifyContent: 'center',
+								}}
+							>
+								<Text style={{ color: 'white', fontSize: scale(20) }}>
+									{elem.key}
+								</Text>
+							</View>
+						)
+					})}
+			/>
+		</SafeAreaView>
+	)
 }
 
 export default RestaurantsScreen
