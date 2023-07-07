5f3dcb2c95601bc4545c4b1b6fc8539e146865bd
ZhSa99 <zhsa.09.99@gmail.com>
2023-07-07 06:52:19 +0600

    some changes

--- src/navigation/BottomTabNavigation/BottomTabNavigation.tsx
+++ src/navigation/BottomTabNavigation/BottomTabNavigation.tsx
@@ -1,4 +1,4 @@
-import { Animated, Dimensions, SafeAreaView, StyleSheet, View } from 'react-native'
+import { Animated, Dimensions, StyleSheet, View } from 'react-native'
 import React, { useRef } from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { BottomTabRootParamList } from '../types'
@@ -7,7 +7,7 @@ import RestaurantsScreen from '../../screens/RestaurantsScreen/RestaurantsScreen
 import ShopsScreen from '../../screens/ShopsScreen/ShopsScreen'
 import BasketScreen from '../../screens/BasketScreen/BasketScreen'
 
-import { scale, verticalScale, vs } from 'react-native-size-matters'
+import { scale, verticalScale } from 'react-native-size-matters'
 import CustomIcon from '../../customElements/CustomIcon'
 import { bottomTabColors, buttonColors } from '../../utils/colors'
 import {
@@ -15,11 +15,8 @@ import {
 	RestaurantsScreen_ICON,
 	ShopsScreen_ICON,
 } from '../../utils/iconsPaths'
-import MainHeader from '../../customElements/MainHeader/MainHeader'
 import BasketModalHandler from '../../customElements/BasketModalHandler/BasketModalHandler'
 import { getColor, tabBarLabel } from '../../utils/getUIElems'
-import SearchModalHandler from '../../customElements/SearchModalHandler/SearchModalHandler'
-import { useSharedValue } from 'react-native-reanimated'
 
 const BottomTab = createBottomTabNavigator<BottomTabRootParamList>()
 
@@ -31,119 +28,122 @@ const BottomTabNavigation = () => {
 	const emptyRef = useRef(new Animated.Value(0)).current
 
 	return (
-		<View
-			style={{ flex: 1, backgroundColor: bottomTabColors.sceneBackgroundColor }}
-		>
-			
 
-			<BottomTab.Navigator
-				screenOptions={{
-					headerShown: false,
-					tabBarStyle: {
-						backgroundColor: bottomTabColors.bottomTabColor,
-						height: verticalScale(70),
-						paddingLeft: scale(15),
-						paddingRight: scale(15),
-						borderTopWidth: 0,
-					},
-				}}
-				sceneContainerStyle={{
+			<View
+				style={{
+					flex: 1,
 					backgroundColor: bottomTabColors.sceneBackgroundColor,
 				}}
 			>
-				<BottomTab.Screen
-					name={'MainScreen'}
-					options={{
-						tabBarIcon: ({ focused }) => {
-							return (
-								<CustomIcon
-									source={MainScreen_ICON}
-									iconStyle={{
-										tintColor: focused
-											? buttonColors.activeColor
-											: buttonColors.inactiveColor,
-									}}
-									iconSize={30}
-									containerStyle={{
-										height: verticalScale(30),
-									}}
-								/>
-							)
+				<BottomTab.Navigator
+					screenOptions={{
+						headerShown: false,
+						tabBarStyle: {
+							backgroundColor: bottomTabColors.bottomTabColor,
+							height: verticalScale(70),
+							paddingLeft: scale(15),
+							paddingRight: scale(15),
+							borderTopWidth: 0,
 						},
-						tabBarLabel: ({ focused }) =>
-							tabBarLabel({
-								color: getColor(focused),
-								label: 'Главная',
-							}),
+					}}
+					sceneContainerStyle={{
+						backgroundColor: bottomTabColors.sceneBackgroundColor,
 					}}
 				>
-					{() => <MainScreen />}
-				</BottomTab.Screen>
-				<BottomTab.Screen
-					name={'RestaurantsScreen'}
-					component={RestaurantsScreen}
-					options={{
-						tabBarIcon: ({ focused }) => {
-							return (
-								<CustomIcon
-									source={RestaurantsScreen_ICON}
-									iconStyle={{
-										tintColor: focused
-											? buttonColors.activeColor
-											: buttonColors.inactiveColor,
-									}}
-									iconSize={30}
-									containerStyle={{
-										height: verticalScale(30),
-									}}
-								/>
-							)
-						},
-						tabBarLabel: ({ focused }) =>
-							tabBarLabel({
-								color: getColor(focused),
-								label: 'Рестораны',
-							}),
-					}}
-				/>
-				<BottomTab.Screen
-					name={'ShopsScreen'}
-					component={ShopsScreen}
-					options={{
-						tabBarIcon: ({ focused }) => {
-							return (
-								<CustomIcon
-									source={ShopsScreen_ICON}
-									iconStyle={{
-										tintColor: focused
-											? buttonColors.activeColor
-											: buttonColors.inactiveColor,
-									}}
-									iconSize={30}
-									containerStyle={{
-										height: verticalScale(30),
-									}}
-								/>
-							)
-						},
-						tabBarLabel: ({ focused }) =>
-							tabBarLabel({
-								color: getColor(focused),
-								label: 'Магазины',
-							}),
-					}}
-				/>
-				<BottomTab.Screen
-					name={'BasketScreen'}
-					component={BasketScreen}
-					options={{
-						tabBarButton: () => (
-							<BasketModalHandler style={{ width: width / 4 }} />
-						),
-					}}
-				/>
-			</BottomTab.Navigator>
-		</View>
+					<BottomTab.Screen
+						name={'MainScreen'}
+						options={{
+							tabBarIcon: ({ focused }) => {
+								return (
+									<CustomIcon
+										source={MainScreen_ICON}
+										iconStyle={{
+											tintColor: focused
+												? buttonColors.activeColor
+												: buttonColors.inactiveColor,
+										}}
+										iconSize={30}
+										containerStyle={{
+											height: verticalScale(30),
+										}}
+									/>
+								)
+							},
+							tabBarLabel: ({ focused }) =>
+								tabBarLabel({
+									color: getColor(focused),
+									label: 'Главная',
+								}),
+						}}
+					>
+						{() => <MainScreen />}
+					</BottomTab.Screen>
+					<BottomTab.Screen
+						name={'RestaurantsScreen'}
+						component={RestaurantsScreen}
+						options={{
+							tabBarIcon: ({ focused }) => {
+								return (
+									<CustomIcon
+										source={RestaurantsScreen_ICON}
+										iconStyle={{
+											tintColor: focused
+												? buttonColors.activeColor
+												: buttonColors.inactiveColor,
+										}}
+										iconSize={30}
+										containerStyle={{
+											height: verticalScale(30),
+										}}
+									/>
+								)
+							},
+							tabBarLabel: ({ focused }) =>
+								tabBarLabel({
+									color: getColor(focused),
+									label: 'Рестораны',
+								}),
+						}}
+					/>
+					<BottomTab.Screen
+						name={'ShopsScreen'}
+						component={ShopsScreen}
+						options={{
+							tabBarIcon: ({ focused }) => {
+								return (
+									<CustomIcon
+										source={ShopsScreen_ICON}
+										iconStyle={{
+											tintColor: focused
+												? buttonColors.activeColor
+												: buttonColors.inactiveColor,
+										}}
+										iconSize={30}
+										containerStyle={{
+											height: verticalScale(30),
+										}}
+									/>
+								)
+							},
+							tabBarLabel: ({ focused }) =>
+								tabBarLabel({
+									color: getColor(focused),
+									label: 'Магазины',
+								}),
+						}}
+					/>
+					<BottomTab.Screen
+						name={'BasketScreen'}
+						component={BasketScreen}
+						options={{
+							tabBarButton: () => (
+								<BasketModalHandler style={{ width: width / 4 }} />
+							),
+						}}
+					/>
+				</BottomTab.Navigator>
+			</View>
+
 	)
 }
 
