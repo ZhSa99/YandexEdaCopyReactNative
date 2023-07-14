61d8a3d0f1416d11be7f51b1300adb2facdcbfc5
ZhSa99 <zhsa.09.99@gmail.com>
2023-07-15 04:33:56 +0600

    refactoring

--- src/screens/RestaurantInfoScreen/RestaurantInfoScreen.tsx
+++ src/screens/RestaurantInfoScreen/RestaurantInfoScreen.tsx
@@ -1,32 +1,9 @@
-import {
-	ImageBackground,
-	SafeAreaView,
-	StyleSheet,
-	Text,
-	View,
-} from 'react-native'
+import { View } from 'react-native'
 import React from 'react'
-import { useNavigation, useRoute } from '@react-navigation/native'
-import { collection, getDocs } from 'firebase/firestore'
-import { db } from '../../firebase/firebaseConfig'
 import useRestaurants, { IRestaurantInfo } from '../../hooks/useRestaurants'
 import { ActivityIndicator } from 'react-native'
-import {
-	buttonContainerColor,
-	containerColor,
-	mainBackgroundColor,
-	textColorDark,
-	textColorLightDark,
-} from '../../utils/colors'
-import { scale, verticalScale } from 'react-native-size-matters'
-import { LinearGradient } from 'expo-linear-gradient'
-import CustomIcon from '../../customElements/CustomIcon'
-import CustomExpoIcon from '../../customElements/CustomExpoIcon'
-import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons'
-import { delivery_ICON } from '../../utils/iconsPaths'
-import { getRatingInfo } from '../../customElements/RestaurantRatingInfo'
-import RestaurantInfo from './components/RestaurantInfo'
-import CategoriesList from './components/CategoriesList'
+import { mainBackgroundColor } from '../../utils/colors'
+import ItemsList from './components/ItemsList'
 
 const RestaurantInfoScreen = ({
 	route,
@@ -42,30 +19,15 @@ const RestaurantInfoScreen = ({
 		getDishesListFromRestaurant(route.params.restaurantInfo.id)
 	}, [])
 
-	const dishesListFiltered = dishesList.filter(
-		(elem) => elem.id !== 'generalInformation'
-	)
-	const categoriesList = Object.values(
-		dishesList.filter((elem) => elem.id === 'generalInformation')[0]
-	).filter(elem => elem !== 'generalInformation')
-	
 	return (
 		<View style={{ backgroundColor: mainBackgroundColor, height: '100%' }}>
 			{isLoading ? (
 				<ActivityIndicator />
 			) : (
-				<>
-					<RestaurantInfo restaurantInfo={restaurantInfo} />
-
-					<View style={{ paddingTop: verticalScale(20) }}>
-						<CategoriesList categoriesList={categoriesList} />
-					</View>
-				</>
+				<ItemsList dishesList={dishesList} restaurantInfo={restaurantInfo} />
 			)}
 		</View>
 	)
 }
 
 export default RestaurantInfoScreen
-
-const styles = StyleSheet.create({})
