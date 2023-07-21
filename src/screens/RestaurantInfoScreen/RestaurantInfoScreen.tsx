fc51f15fb9d9f159238f24bb63f7980928f323e6
ZhSa99 <zhsa.09.99@gmail.com>
2023-07-21 23:29:04 +0600

    add custom animated indicator

--- src/screens/RestaurantInfoScreen/RestaurantInfoScreen.tsx
+++ src/screens/RestaurantInfoScreen/RestaurantInfoScreen.tsx
@@ -1,9 +1,10 @@
-import { View } from 'react-native'
+import { Button, SafeAreaView, View } from 'react-native'
 import React from 'react'
 import useRestaurants, { IRestaurantInfo } from '../../hooks/useRestaurants'
 import { ActivityIndicator } from 'react-native'
 import { mainBackgroundColor } from '../../utils/colors'
 import ItemsList from './components/ItemsList'
+import YandexLoadingAnimation from '../../customElements/YandexLoadingAnimation'
 
 const RestaurantInfoScreen = ({
 	route,
@@ -21,8 +22,11 @@ const RestaurantInfoScreen = ({
 
 	return (
 		<View style={{ backgroundColor: mainBackgroundColor, height: '100%' }}>
+			
 			{isLoading ? (
-				<ActivityIndicator />
+				<SafeAreaView>
+					<YandexLoadingAnimation />
+				</SafeAreaView>
 			) : (
 				<ItemsList dishesList={dishesList} restaurantInfo={restaurantInfo} />
 			)}
